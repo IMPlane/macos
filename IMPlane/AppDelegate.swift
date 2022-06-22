d30d0a89121bc9de8772d6117f64250dac827398
Hiroki.T <hirodora@me.com>
2022-06-22 18:39:23 +0900

    add: IMKit Server

--- IMPlane/AppDelegate.swift
+++ IMPlane/AppDelegate.swift
@@ -6,6 +6,47 @@
 //
 
 import Cocoa
+import InputMethodKit
+
+
+
+/// 文字列がPrintableかどうかチェックする
+/// - Parameter text: 検証したい文字列
+/// - Returns: PrintableならTrue, そうでなければFalse
+func isPrintable(_ text: String) -> Bool {
+    let printable = [
+        CharacterSet.alphanumerics,
+        CharacterSet.symbols,
+        CharacterSet.punctuationCharacters
+    ].reduce(CharacterSet(), { $0.union($1) } )
+
+    return !text.unicodeScalars.contains { !printable.contains($0) }
+}
+
+
+
+@objc(IMPlaneController)
+class IMPlaneController: IMKInputController {
+    public override init!(server: IMKServer!, delegate: Any!, client inputClient: Any!) {
+        super.init(server: server, delegate: delegate, client: inputClient)
+    }
+    
+    public override func handle(_ event: NSEvent, client: Any) -> Bool {
+        guard let client = client as? IMKTextInput else {
+            return false
+        }
+
+        if let text = event.characters, isPrintable(text) {
+            let cursor = NSRange(location: NSNotFound, length: NSNotFound)
+            client.insertText(text.uppercased(), replacementRange: cursor)
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -14,7 +55,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let server = IMKServer(name: "net.kekeho.inputmethod.IMPlane_Connection", bundleIdentifier: Bundle.main.bundleIdentifier)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
